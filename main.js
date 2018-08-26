d506782ae72f515efa29b551175380424a373a1f
MarcoWorms <marcogworms@gmail.com>
2018-08-26 00:24:38 -0300

    animations will not stop me

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+window.beforeunload = () => {
+  window.scrollTo(0, 0)
+}
+
 window.onload = () => {
 
 function lerp(v0, v1, t) {
@@ -22,6 +26,32 @@ const ilustras = {
   thirdBig: document.querySelector('.animation-3'),
   fourthBig: document.querySelector('.animation-4'),
 }
+
+const towers = [
+  document.querySelector('.t1'),
+  document.querySelector('.t2'),
+  document.querySelector('.t3'),
+  document.querySelector('.t4'),
+  document.querySelector('.t5'),
+  document.querySelector('.t6'),
+]
+
+const towersInitialXY = towers.map(tower => {
+  return {
+    x: parseInt(
+      window
+        .getComputedStyle(tower)
+        .getPropertyValue('transform')
+        .split(',')[4]
+    ),
+    y: parseInt(
+      window
+        .getComputedStyle(tower)
+        .getPropertyValue('transform')
+        .split(',')[5]
+    ),
+  }
+})
 
 const startY = -50
 
@@ -50,6 +80,7 @@ Promise.resolve()
   scrollTriggered()
 })
 
+var thirdSquareShown = false
 var secondSquareShown = false
 var firstSquareShown = true
 window.onscroll = scrollTriggered
@@ -96,20 +127,44 @@ function secondSquareTest (scroll, opacity, translateY) {
     .then(() => { document.querySelector('.bg2-3').style.transform = `translate(20px, 0)` })
 }
 
+function thirdSquareTest (scroll, opacity, translateY) {
+  thirdSquareShown = !!opacity
+  console.log(opacity)
+  Promise.resolve()
+    .then(wait(75))
+    .then(() => { document.querySelector('.box1-6').style.opacity = opacity })
+    .then(() => { document.querySelector('.box1-6').style.transform = `translate(0, ${opacity * 10}px)` })
+    .then(wait(75))
+    .then(() => { document.querySelector('.box2-6').style.opacity = opacity })
+    .then(() => { document.querySelector('.box2-6').style.transform = `translate(0, ${opacity * 10}px)` })
+    .then(wait(75))
+    .then(() => { document.querySelector('.box3-6').style.opacity = opacity })
+    .then(() => { document.querySelector('.box3-6').style.transform = `translate(0, ${opacity * 10}px)` })
+    .then(wait(75))
+    .then(() => { document.querySelector('.box4-6').style.opacity = opacity })
+    .then(() => { document.querySelector('.box4-6').style.transform = `translate(0, ${opacity * 10}px)` })
+    .then(wait(75))
+    .then(() => { document.querySelector('.box5-6').style.opacity = opacity })
+    .then(() => { document.querySelector('.box5-6').style.transform = `translate(0, ${opacity * 10}px)` })
+    .then(wait(75))
+    .then(() => { document.querySelector('.box6-6').style.opacity = opacity })
+    .then(() => { document.querySelector('.box6-6').style.transform = `translate(0, ${opacity * 10}px)` })
+
+  Promise.resolve()
+    .then(wait(100))
+    .then(() => { document.querySelector('.bg3-1').style.opacity = 1 })
+    .then(() => { document.querySelector('.bg3-1').style.transform = `translate(20px, 0)` })
+    .then(wait(100))
+    .then(() => { document.querySelector('.bg3-2').style.opacity = 1 })
+    .then(() => { document.querySelector('.bg3-2').style.transform = `translate(20px, 0)` })
+}
+
 function scrollTriggered () {
   const scroll = window.scrollY
 
-  // if (
-  //   scroll < 700
-  // ) {
   if (!firstSquareShown) {
     firstSquareTest(scroll, 1, 20)
   }
-  // } else {
-  //   if (firstSquareShown) {
-  //     firstSquareTest(scroll, 0, 0)
-  //   }
-  // }
 
   if (
     scroll > scrollSteps[1] - 100 &&
@@ -121,6 +176,18 @@ function scrollTriggered () {
   } else {
     if (secondSquareShown) {
       secondSquareTest(scroll, 0, 0)
+    }
+  }
+  if (
+    scroll > scrollSteps[2] - 100 &&
+    scroll < scrollSteps[2] + 100
+  ) {
+    if (!thirdSquareShown) {
+      thirdSquareTest(scroll, 1, 20)
+    }
+  } else {
+    if (thirdSquareShown) {
+      thirdSquareTest(scroll, 0, 0)
     }
   }
 
@@ -138,19 +205,45 @@ function scrollTriggered () {
   }
   const t1 = (scroll - scrollSteps[0]) / (scrollSteps[1] - scrollSteps[0])
   const t2 = (scroll - scrollSteps[1]) / (scrollSteps[2] - scrollSteps[1])
-  const t3 = (scroll - scrollSteps[2]) / (scrollSteps[3] - scrollSteps[2])
+  const t3 = (scroll - scrollSteps[2]) / (scrollSteps[4] - scrollSteps[2])
   const t4 = (scroll - scrollSteps[3]) / (scrollSteps[4] - scrollSteps[3])
+
+  document.querySelector('.home').className =
+    document.querySelector('.home').className
+      .split(' ').filter(className => className !== 'blue-color-only').join(' ')
+  document.querySelector('.about').className =
+    document.querySelector('.about').className
+      .split(' ').filter(className => className !== 'blue-color-only').join(' ')
+  document.querySelector('.specs').className =
+    document.querySelector('.specs').className
+      .split(' ').filter(className => className !== 'blue-color-only').join(' ')
+  document.querySelector('.contact').className =
+    document.querySelector('.contact').className
+      .split(' ').filter(className => className !== 'blue-color-only').join(' ')
+  if (t4 >= 0) {
+    document.querySelector('.contact').className += ' blue-color-only'
+  }
+  else if (t3 >= 0) {
+    document.querySelector('.specs').className += ' blue-color-only'
+  }
+  else if (t2 >= 0) {
+    document.querySelector('.about').className += ' blue-color-only'
+  }
+  else if (t1 >= 0) {
+    document.querySelector('.home').className += ' blue-color-only'
+  }
 
   firstAnimation(t1, t2, t3, t4)
   secondAnimation(t1, t2, t3, t4)
   thirdAnimation(t1, t2, t3, t4)
   fourthAnimation(t1, t2, t3, t4)
+  animateTowers(t1, t2, t3, t4)
 }
 
 function firstAnimation (t1, t2, t3, t4) {
   if (t1 < 1) {
     ilustras.firstBig.style.transform = `translate(0, ${
-      lerp(startY, -1500, t1)
+      lerp(startY, -1000, t1)
     }px)`
     return
   }
@@ -166,11 +259,12 @@ function secondAnimation (t1, t2, t3, t4) {
     return
   }
   ilustras.secondBig.style.transform = `translate(0, ${
-    lerp(250, -1500, t2)
+    lerp(250, -1000, t2)
   }px)`
 }
 
 function thirdAnimation (t1, t2, t3, t4) {
+
   if (t1 < 1) {
     ilustras.thirdBig.style.transform = `translate(0, ${
       lerp(startY, 250, t1 - 0.2 * (1 - t1) + 0.15 * (t1))
@@ -179,13 +273,45 @@ function thirdAnimation (t1, t2, t3, t4) {
   }
   if (t2 < 1) {
     ilustras.thirdBig.style.transform = `translate(0, ${
-      lerp(250, 210, t2 - 0.01 * (1 - t2) + 0.4 * (t2))
+      lerp(250, 180, t2 - 0.2 * (1 - t2) + 0.5 * (t2))
     }px)`
     return
   }
   ilustras.thirdBig.style.transform = `translate(0, ${
-    lerp(210, -1500, t3)
+    lerp(180, -1000, t3)
   }px)`
+}
+
+const towerMiddleXY = [
+  { x: -316, y: -324, },
+  { x: -316, y: -125, },
+  { x: -40, y: -125, },
+  { x: -40, y: -324, },
+  { x: 238, y: -324, },
+  { x: 238, y: -125, },
+]
+function animateTowers (t1, t2, t3, t4) {
+  towers.forEach((tower, index) => {
+    const { x, y } = towersInitialXY[index]
+    const { x: x2, y: y2 } = towerMiddleXY[index]
+    if (t1 <= 1) {
+      tower.style.transform = `translate(${x}px, ${
+        lerp(y + 10, 318 + y, t1 - 0.2 * (1 - t1) + 0.15 * (t1))
+      }px)`
+      return
+    }
+    if (t2 <= 1) {
+      tower.style.transform = `translate(${
+        lerp(x, x2, t2)
+      }px, ${
+        lerp(318 + y, y2, t2)
+      }px)`
+      return
+    }
+    tower.style.transform = `translate(${x2}px, ${
+        lerp(y2, -1500, t3)
+    }px)`
+  })
 }
 
 function fourthAnimation (t1, t2, t3, t4) {
@@ -197,12 +323,12 @@ function fourthAnimation (t1, t2, t3, t4) {
   }
   if (t2 < 1) {
     ilustras.fourthBig.style.transform = `translate(0, ${
-      lerp(250, 210, t2 - 0.6 * (1 - t2) + 0.05 * (t2))
+      lerp(250, 180, t2 - 0.3 * (1 - t2) + 0.05 * (t2))
     }px)`
     return
   }
   ilustras.fourthBig.style.transform = `translate(0, ${
-    lerp(210, 0, t3)
+    lerp(180, -100, t3)
   }px)`
 }
 if (window.scrollY !== 0) {

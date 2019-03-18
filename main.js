9f16806d3896f0d952b85601fd356ce02392bf43
MarcoWorms <marcogworms@gmail.com>
2019-03-18 16:21:15 -0300

    change pipefy form to salesforce

--- main.js
+++ main.js
@@ -390,11 +390,11 @@ window.addEventListener('scroll', () => {
 	}, 66)
 }, false)
 
-const frame = document.createElement('iframe')
-frame.src = 'https://app.pipefy.com/public/form/Gp5ojRgJ?embedded=true'
-frame.frameBorder = 0
-frame.classList = 'pipefy-form' 
-document.querySelector('.bg-squares-4 .pipe').appendChild(frame)
+// const frame = document.createElement('iframe')
+// frame.src = 'https://app.pipefy.com/public/form/Gp5ojRgJ?embedded=true'
+// frame.frameBorder = 0
+// frame.classList = 'pipefy-form' 
+// document.querySelector('.bg-squares-4 .pipe').appendChild(frame)
 
 
 const checkSize = e => {
